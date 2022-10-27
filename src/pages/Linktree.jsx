1cd6bb80e3d9ab9d29f0621dc2e574037c9fec6f
ibimina <96787210+ibimina@users.noreply.github.com>
2022-10-27 22:36:10 +0100

    Wrapped component html with a div class container

--- src/pages/Linktree.jsx
+++ src/pages/Linktree.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Footer, LinksBtn, Profile } from '../components'
-
+import"./linktree.css"
 export default function Linktree() {
   return (
-    <>
-      <div>
+    <div className='container'>
+      <div >
         <Profile />
         <LinksBtn />
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
