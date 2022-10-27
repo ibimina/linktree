a89e8575c5d03c85097344d12a8eecf44d3d7dca
ibimina <96787210+ibimina@users.noreply.github.com>
2022-10-27 11:52:18 +0100

    Added components file and app pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-
+  const name = "Annette black";
+  const links = [
+    "twitter link",
+    "zuri team",
+    "zuri books",
+    "python books",
+    "background check for coders",
+    "design books",
+  ];
 
   return (
-    <div className="App">  
-    <p>hello</p>
+    <div className="App">
+      <p>hello</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Linktree.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Linktree() {
+  return (
+    <div>Linktree</div>
+  )
+}
