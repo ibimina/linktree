6edf683e38cfdc3acc642c12a86ae5455d79dc6b
ibimina <96787210+ibimina@users.noreply.github.com>
2022-10-27 12:54:37 +0100

    Added profile twitter and slack username

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Profile() {
   return (
-    <section className='profile_section'>
-        <img src="/assets/device=Mobile, state=Default.png" alt="Annette Black" id='profile_img'/>
-        <p>Annette Black</p>
+    <section className="profile_section">
+      <img
+        src="/assets/device=Mobile, state=Default.png"
+        alt="Annette Black"
+        id="profile_img"
+      />
+      <button className="share"></button>
+      <p id="twitter">IbiminaaH</p>
+      <p id="ibimina" hidden>
+        ibimina
+      </p>
     </section>
-  )
+  );
 }
