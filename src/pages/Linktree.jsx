7b5a523ee8af82c25eb97d45b579f2d8f62608c8
ibimina <96787210+ibimina@users.noreply.github.com>
2022-10-27 12:55:12 +0100

    import components

--- src/pages/Linktree.jsx
+++ src/pages/Linktree.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Footer, LinksBtn, Profile } from '../components'
 
 export default function Linktree() {
   return (
-    <div>Linktree</div>
-  )
+    <>
+      <div>
+        <Profile />
+        <LinksBtn />
+      </div>
+      <Footer />
+    </>
+  );
 }
