9911b078e1d2dfbb2a3fa6d91cc0354f370b2296
ibimina <96787210+ibimina@users.noreply.github.com>
2022-10-28 09:57:36 +0100

    added  cam icon

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,7 +4,9 @@ export default function Profile() {
   return (
     <section className="profile_section">
       <div>
-        <div id="profile_img"></div>
+        <div id="profile_img">
+           <img src="./assets/cam.svg" alt="" className="cam" />
+        </div>
         <p id="twitter">IbiminaaH</p>
         <p id="ibimina" hidden>
           ibimina
@@ -15,8 +17,13 @@ export default function Profile() {
           <span>Share link</span>
           <img src="./assets/bottom-center.svg" alt="" className="arrow" />
         </div>
-
-        <button className="share"></button>
+        <picture className="share">
+          <source
+            srcset="./assets/share-default.svg"
+            media="(min-width:40rem)"
+          />
+          <img src="./assets/default-dot.svg" alt="" />
+        </picture>
       </div>
     </section>
   );
